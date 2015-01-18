92c8673a4a1968e19804e9b79e59d3c8da6a5c7d
Jan Schulte <hello@unexpected-co.de>
2015-01-18 16:15:24 +0100

    Add Desktop class for changing a wallpaper

---
+++ werbetafel/WTDesktop.h
@@ -0,0 +1,13 @@
+//
+// Created by Jules Winnfield on 18/01/15.
+// Copyright (c) 2015 nein. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface WTDesktop : NSObject
+
+-(void) setNewWallpaper: (NSURL *) wallpaper;
+
+@end
---
+++ werbetafel/WTDesktop.m
@@ -0,0 +1,38 @@
+//
+// Created by Jules Winnfield on 18/01/15.
+// Copyright (c) 2015 nein. All rights reserved.
+//
+
+#import "WTDesktop.h"
+#import <Cocoa/Cocoa.h>
+
+@implementation WTDesktop
+
+-(void) setNewWallpaper: (NSURL *) wallpaper {
+    NSWorkspace *sws = [NSWorkspace sharedWorkspace];
+
+    NSError *err = nil;
+    for (NSScreen *screen in [NSScreen screens]) {
+        NSDictionary *opt = [sws desktopImageOptionsForScreen:screen];
+        [sws setDesktopImageURL:wallpaper forScreen:screen options:opt error:&err];
+        if (err) {
+            NSLog(@"%@",[err localizedDescription]);
+        }else{
+            NSNumber *scr = [screen deviceDescription][@"NSScreenNumber"];
+            NSLog(@"Set %@ for space %i on screen %@",[wallpaper path],[self spaceNumber],scr);
+        }
+    }
+}
+
+-(int)spaceNumber
+{
+    CFArrayRef windowsInSpace = CGWindowListCopyWindowInfo(kCGWindowListOptionAll | kCGWindowListOptionOnScreenOnly, kCGNullWindowID);
+    for (NSMutableDictionary *thisWindow in (__bridge NSArray *)windowsInSpace)    {
+        if (thisWindow[(id) kCGWindowWorkspace]){
+            return [thisWindow[(id) kCGWindowWorkspace] intValue];
+        }
+    }
+    return -1;
+}
+
+@end
